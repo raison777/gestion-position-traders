235982363d5067748d0e54f59ad7a0c8362b88d1
sorian.slimani@viacesi.fr <sorian.slimani@viacesi.fr>
2015-08-04 12:13:33 +0200

    Mise en place du traderService part 1 & Redaction de MyFeelings.txt

---
+++ gestion-position-traders-project/app/services/impl/trader_service.rb
@@ -0,0 +1,17 @@
+class TraderService
+  def update_trader(trader)
+    trader.save()
+  end
+
+  def register_trader(name)
+    Trader.create(:name => name)
+  end
+
+  def delete_trader(trader)
+    trader.destroy
+  end
+
+  def find_by_name(name)
+    Trader.find_by name: name
+  end
+end
